fbb6f73da3a52fd8e62cbd95b969da8a8d88021a
Xiza73 <m.fajardo.1098@gmail.com>
2025-04-22 01:44:00 -0500

    chore: fix get transaction categories and services

--- src/api/transaction/domain/responses/get-transaction-services.response.ts
+++ src/api/transaction/domain/responses/get-transaction-services.response.ts
@@ -2,11 +2,12 @@ import { z as zod } from 'zod';
 
 import { z } from '@/config/zod.config';
 import { ServiceResponse, ServiceResponseSchema } from '@/domain/service-response.model';
+import { commonValidations } from '@/utils/common-validation.util';
 
 export const GetTransactionServicesResponseObject = z.object({
   data: z.array(
     z.object({
-      id: z.number(),
+      id: commonValidations.toValidNumber,
       name: z.string(),
     })
   ),
